e9be94d8b5f349fd7b5cecc799dc5d01545e7ebf
Bruno Leonardo <bruno.lsilva1508@gmail.com>
2026-04-17 20:50:25 -0300

    Initial Commit

---
+++ WeSplitApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WeSplitApp
+//
+//  Created by Bruno Silva on 17/04/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WeSplitApp/WeSplitAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitAppApp.swift
+//  WeSplitApp
+//
+//  Created by Bruno Silva on 17/04/26.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
